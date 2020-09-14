5c1434c8db02dbd1c1b8ef68ffabdcc91b70721b
Poyser <obryan.poyser@sanofi.com>
2020-09-14 10:42:01 +0200

    testing dupixent

--- test_source/internal_test.R
+++ test_source/internal_test.R
@@ -2,7 +2,7 @@
 
 pkg <- c("glmnet", "forecast", "stlplus", "fastDummies", "imputeTS", "plotly"
          , "tidyverse", "doParallel", "foreach", "parallel", "tsibble", "doSNOW"
-         , "forecTheta")
+         , "forecTheta", "prophet")
 
 invisible(lapply(pkg, require, character.only = TRUE))
 
@@ -24,25 +24,26 @@ source("R/feature_engineering.R")
 source("R/optim_ts.R")
 source("R/get_dyn_theta.R")
 source("R/get_tslm.R")
+source("R/get_prophet.R")
 
 # Parameter ---------------------------------------------------------------
 
 grid_glmnet <- expand_grid(time_weight = seq(from = 0.9, to = 1, by = 0.02)
                            , trend_discount = seq(from = 0.95, to = 1, by = 0.01)
                            , alpha = seq(from = 0, to = 1, by = 0.10))
-grid_glm <- expand_grid(time_weight = seq(from = 0.8, to = 1, by = 0.02)
-                        , trend_discount = seq(from = 0.8, to = 1, by = 0.02))
+grid_glm <- expand_grid(time_weight = seq(from = 0.9, to = 1, by = 0.02)
+                        , trend_discount = seq(from = 0.9, to = 1, by = 0.02))
 
-parameter <- list(glmnet = list(time_weight = .94, trend_discount = .70, alpha = 0, lambda = .1
+parameter <- list(glmnet = list(time_weight = 1.0, trend_discount = .91, alpha = .7, lambda = 320
                                 , grid_glmnet = grid_glmnet
-                                , job = list(optim_lambda = TRUE, x_excluded = NULL
-                                             , random_search_size = 0.05
+                                , job = list(optim_lambda = FALSE, x_excluded = NULL
+                                             , random_search_size = 0.1
                                              , n_best_model = 1))
                   , croston = list(alpha = 0.1)
-                  , glm = list(time_weight = .99, trend_discount = 0.70
+                  , glm = list(time_weight = 1.0, trend_discount = .91
                                , grid_glm = grid_glm
                                , job = list(x_excluded = NULL
-                                            , random_search_size = 0.1
+                                            , random_search_size = 0.3
                                             , n_best_model = 1))
                   , arima = list(p = 1, d = 1, q = 0, P = 1, D = 0, Q = 0)
                   , ets = list(ets = "ZZZ"))
@@ -56,6 +57,15 @@ data_init <- read_csv("test_source/demo_data.csv") %>%
 data_all <- data_init %>%
   prescribe_ts(key = "forecast_item", y_var = "volume", date_var = "date"
                , freq = 12, reg_name = "reg_name", reg_value = "reg_value")
+
+
+## Dupixent
+
+us0 <- read_rds("test_source/us_dupixent.rds") %>% 
+  mutate(reg_name = case_when(
+    year_month == "2019-12-01" & key == "US: 710613" ~ "christmas"
+    , TRUE ~ reg_name)
+    , reg_value = ifelse(year_month == "2019-12-01" & key == "US: 710613", 1, reg_value))
 
 # Single item forecast / modularity ---------------------------------------
 
@@ -88,34 +98,59 @@ data_all %>%
 ## Optimization ------------------------------------------------------------
 
 model_list  <- c("glm", "glmnet", "neural_network", "arima", "ets", "seasonal_naive", "croston","tbats","dynamic_theta","tslm")
+model_list  <- c("glm", "glmnet")
 
-.data <- data_all %>% 
-  dplyr::filter(key == "FI: 515188")
+
+dupi <- read_excel("test_source/dupixent_us.xlsx") %>% 
+  mutate(date = as.Date(date))
+
+us_all <- dupi %>% 
+  filter(date < "2020-01-01") %>% 
+  prescribe_ts(key = "key", y_var = "volume", date_var = "date"
+               , reg_name = "reg_name", reg_value = "reg_value", freq = 12)
+
+.data <- us_all %>% 
+  dplyr::filter(key == "US: 710613") %>% 
+  feature_engineering_ts() %>% 
+  clean_ts(method = "kalman")
+
+  
+  optim_ts(test_size = 3, lag = 1, parameter = parameter, model = "glmnet")
+  fit_ts(model = "glmnet", parameter = parameter) %>% 
+  get_forecast(horizon = 12)
+  ggplot(aes(date_var, y_var))+
+  geom_line()
 
 fast_optim_forecast <- autoforecast(.data = .data
-                                    , horizon = 12
-                                    , model = setdiff(model_list, c("tbats"))
+                                    , horizon = 6
+                                    , model = model_list
                                     , parameter = parameter
                                     , optim_profile = "fast"
                                     , method = "kalman"
                                     , number_best_models = 3
-                                    , pred_interval = TRUE)
+                                    , pred_interval = FALSE)
 fast_optim_forecast %>% 
   plot_ts(interactive = T)
 
+fast_optim_forecast %>% 
+  clipr::write_clip()
+
 ## Light
+
+
+
 
 set.seed(1)
 my_cores <- detectCores()
 registerDoParallel(cores = (my_cores - 2))
 
 light_optim_forecast <- autoforecast(.data = .data
-                                     , horizon = 100
+                                     , horizon = 6
                                      , model = model_list
                                      , parameter = parameter
                                      , optim_profile = "light"
                                      , test_size = 6
-                                     , lag = 3
+                                     , lag = 1
                                      , meta_data = FALSE
                                      , tune_parallel = FALSE
                                      , pred_interval = FALSE
@@ -127,7 +162,7 @@ light_optim_forecast %>%
 
 
 
-
+clipr::write_clip(light_optim_forecast=
 
 
 
@@ -143,10 +178,10 @@ progress <- function(n) {
 }
 opts <- list(progress=progress)
 
-results <- foreach(key_i = unique(data_all$key)[1:2]
+results <- foreach(key_i = unique(us$key)
                    , .combine = "rbind"
                    , .options.snow=opts, .packages = pkg) %dopar% {
-  data_i <- data_all[data_all$key == key_i,]
+  data_i <- us[us$key == key_i,]
   autoforecast(.data = data_i, horizon = 100
                , model = model_list
                , parameter = parameter, optim_profile = "light", test_size = 6
