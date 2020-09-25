fba0104be46f3ce1e309118ae3e7c588b165c075
Poyser <obryan.poyser@sanofi.com>
2020-09-25 10:20:20 +0200

    improving prescribe heuristics handling keys and reg_name and reg_var

--- R/cleansing.R
+++ R/cleansing.R
@@ -213,12 +213,36 @@ impute_ts <- function(.data, y_var, method="winsorize", na_exclude = NULL, freq 
 #' \dontrun{
 #' prescribe_ts()
 #' }
-prescribe_ts <- function(.data, key, y_var, date_var, reg_name = NULL, reg_value = NULL, freq){
+prescribe_ts <- function(.data, key = NULL, y_var, date_var, reg_name = NULL, reg_value = NULL, freq){
+  # basic configuration
   attr(.data, "prescription") <- list(key = "key", y_var = "y_var"
                                       , date_var = "date_var", freq = "freq"
                                       , max_date = as.Date("1970-01-01"))
-  .data_tmp <- .data %>% 
-    rename("key" = key, "y_var" = y_var, "date_var" = date_var)
+  
+  if(is.null(key)==TRUE){
+    if(any(duplicated(.data[[date_var]])) == TRUE){
+      stop("A duplicated date index has been found and no key column has been defined, please review the input data.")
+    } else {
+      .data <- .data %>% 
+        mutate(key = "key_001")
+    }
+  }
+  
+  if(is.null(reg_name) == T | is.null(reg_value) == T){
+    .data_tmp <- .data %>% 
+      mutate(reg_name = NA_character_, reg_value = 0)
+  } else {
+    .data_tmp <- .data %>% 
+      rename("reg_name" = reg_name, "reg_value" = reg_value)
+    attr(.data_tmp, "prescription")[["reg_name"]] <- "reg_name"
+    attr(.data_tmp, "prescription")[["reg_value"]] <- "reg_value"
+    attr(.data_tmp, "prescription")[["has_regressors"]] <- TRUE
+    prescription <- attributes(.data_tmp)[["prescription"]]
+  }
+  
+  .data_tmp <- .data_tmp %>% 
+    rename("key" = key, "y_var" = y_var, "date_var" = date_var) %>% 
+    dplyr::select(key, date_var, y_var, everything())
   
   attr(.data_tmp, "prescription")[["y_var"]] <- "y_var"
   attr(.data_tmp, "prescription")[["freq"]] <- freq
@@ -228,15 +252,6 @@ prescribe_ts <- function(.data, key, y_var, date_var, reg_name = NULL, reg_value
   attr(.data_tmp, "prescription")[["multiple_keys"]] <- n_distinct(.data_tmp[["key"]])>1
   
   prescription <- attributes(.data_tmp)[["prescription"]]
-  
-  if(is.null(reg_name) == FALSE & is.null(reg_value) == FALSE){
-    .data_tmp <- .data_tmp %>% 
-      rename("reg_name" = reg_name, "reg_value" = reg_value)
-    attr(.data_tmp, "prescription")[["reg_name"]] <- "reg_name"
-    attr(.data_tmp, "prescription")[["reg_value"]] <- "reg_value"
-    attr(.data_tmp, "prescription")[["has_regressors"]] <- TRUE
-    prescription <- attributes(.data_tmp)[["prescription"]]
-  }
   
   return(.data_tmp)
 }
