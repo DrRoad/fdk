2f09aeafa18e190ce2c4a17be9d3bd34600c904d
opoyc <opoyserc@gmail.com>
2021-02-15 12:20:07 +0100

    adding log capabilities

--- R/data_validation.R
+++ R/data_validation.R
@@ -9,10 +9,12 @@
 #'
 validate_ts <- function(.data, na_values = list(y_var = 0, reg_value = 0, reg_name = "")){
   
+  .data_tmp <- .data
+  
   fill_ts <- function(.data){
     seq_complete <- seq.Date(from = as.Date(min(.data[["date_var"]]))
                              , to = as.Date(max(.data[["date_var"]]))
-                             , by = af_log$log[[c(1,3)]])
+                             , by = .log_init$prescription$freq_name)
     n_missing_dates <- (length(seq_complete) - length(.data[["date_var"]]))
     missing_dates <- base::setdiff(as.character(seq_complete), as.character(.data[["date_var"]]))
     dates_with_reg <- .data %>% 
@@ -38,17 +40,19 @@ validate_ts <- function(.data, na_values = list(y_var = 0, reg_value = 0, reg_na
         }
       }
       
-      log_duplicates <- update_logger(key = attributes(.data)[["key"]]
-                               , module = "date_duplication"
-                               , new_log = tibble(duplicated_dates = list(duplicated_dates)
-                                                  , missing_dates = list(missing_dates)
-                                                  , dates_with_reg = list(dates_with_reg)
-                                                  , warning = warning_log
-                                                  , solution = solution_log))
+      log_update(module = "dates_check"
+                 , key = attributes(.data)[["key"]]
+                 , new_log = list(n_dates = length(seq_complete)
+                                  , duplicated_dates = duplicated_dates
+                                  , missing_dates = missing_dates
+                                  , dates_with_reg = dates_with_reg
+                                  , warning = warning_log
+                                  , solution = solution_log))
     } else {
-      log_duplicates <- update_logger(key = attributes(.data)[["key"]]
-                                      , module = "date_duplication"
-                                      , new_log = tibble(solution = "No duplicated dates have been found"))
+      log_update(module = "dates_check"
+                 , key = attributes(.data)[["key"]]
+                 , new_log = list(n_dates = length(seq_complete)
+                                  , warning = "No duplicated dates have been found."))
     }
     
     # Output ------------------------------------------------------------------
@@ -73,23 +77,7 @@ validate_ts <- function(.data, na_values = list(y_var = 0, reg_value = 0, reg_na
       dplyr::select(-name) %>% 
       structure("key" = attributes(.data)[["key"]])
     
-    log_meta <- update_logger(key = attributes(tmp)[["key"]]
-                  , module = "gap_filling"
-                  , new_log = tibble(size = nrow(tmp)
-                                     , n_missing_dates = n_missing_dates
-                                     , intermittency = round(sum(tmp[["y_var"]]==0)/length(tmp[["y_var"]]), 2)
-                                     , tail_zero = sum(cumsum(rev(tmp$y_var))==0)
-                                     , excluded_models = c("glmnet")
-                                     , date_min = as.character(min(tmp[["date_var"]]))
-                                     , date_max = as.character(max(tmp[["date_var"]]))
-                                     , has_reg = ifelse(!all(tmp[["reg_name"]] %in% ""), T, F)
-                  #, env = ls(environment())
-                  )
-    )
-    
-    
-    
-    list(.data = tmp, .log = bind_rows(log_meta, log_duplicates))
+    return(tmp)
   }
   
   .data %>% 
