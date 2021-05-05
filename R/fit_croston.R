fd537d17b9bd5394b266944ef27ad34ad78d9996
Poyser <obryan.poyser@sanofi.com>
2021-05-05 11:26:32 +0200

    croston model logic optimization

--- R/fit_croston.R
+++ R/fit_croston.R
@@ -1,9 +1,7 @@
 #' Fit Croston model
 #'
 #' @param .data Data frame or tibble with a response variable.
-#' @param y_var String. Column name of the time series to be forecasted.
-#' @param horizon Numeric. Number of periods to forecast.
-#' @param parameter List. Parameter to be used for estimation, looks for alpha parameter.
+#' @param parameter list: Combination of parameter to estimate the model.
 #'
 #' @import forecast
 #' @import stats
@@ -15,18 +13,19 @@
 #' get_croston()
 #' }
 fit_croston <- function(.data, parameter = NULL){
+  globalVariables(c(".log"))
   horizon <- 1
   # Ts object
-  y_var_int <- ts(.data[["y_var"]], frequency = .log_init$prescription$freq)
+  y_var_int <- ts(.data[["y_var"]], frequency = .log$prescription$freq)
   # Fit
   if(is.null(parameter)){
-    model_fit <- round(as.numeric(croston(y_var_int, h = horizon)[1]), 2)
+    model_fit <- croston(y_var_int, h = horizon)
   }else{
-    model_fit <- round(as.numeric(croston(y_var_int
-                                          , alpha = parameter[["croston"]][["alpha"]]
-                                          , h = horizon)[1]), 2)
+    model_fit <- croston(y_var_int
+                         , alpha = parameter[["croston"]][["alpha"]]
+                         , h = horizon)
   }
   # Output
-  class(model_fit) <- "num_fit"
-  model_fit
+  #class(model_fit) <- "num_fit"
+  return(model_fit)
 }
