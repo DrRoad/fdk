7c26ccb852e1f8dbff5f7105093eb79f3bc9268b
Poyser <obryan.poyser@sanofi.com>
2021-05-05 11:26:05 +0200

    ARIMA fitting optimization

--- R/fit_arima.R
+++ R/fit_arima.R
@@ -1,12 +1,10 @@
 #' Fit Auto Regressive Integrated Moving Average Model
 #'
 #' @param .data Data frame or tibble with a response variable.
-#' @param y_var String. Column name of the time series to be forecasted.
-#' @param is_seasonal Logical. Defines if the optimization takes into account seasonal parameters.
 #' @param parameter List. Combination of parameter to estimate the model.
-#' @param freq Numeric. Time series frequency.
 #'
 #' @import forecast
+#' @importFrom utils globalVariables
 #' @return data-frame
 #' @export
 #'
@@ -15,21 +13,34 @@
 #' get_arima()
 #' }
 fit_arima <- function(.data, parameter = NULL){
+  globalVariables(c(".log"))
+
   freq <- .log$prescription$freq
+  
   y_var_int <- ts(.data[["y_var"]], frequency = freq) # maybe not optimal
-  if(parameter[["arima"]]$auto_arima == TRUE){
-    message("ARIMA optimization...")
-    arima_fit <- auto.arima(y_var_int, stepwise = TRUE
-                            , seasonal = parameter[["arima"]][["search_seasonal"]])
-  } else if(any(is.na(parameter[["arima"]]$pdq[4:6])) == FALSE){
-    arima_fit <- suppressWarnings(Arima(y_var_int
-                                        , order = parameter[["arima"]]$pdq[1:3]
-                                        , seasonal = parameter[["arima"]][["pdq"]][4:6]
-                                        , method = "ML"))
-  } else {
-    arima_fit <- suppressWarnings(Arima(y_var_int
-                                        , order = parameter[["arima"]]$pdq[1:3]
-                                        , method = "ML"))
-  }
+  
+  arima_fit <- tryCatch(
+    {
+      if(parameter[["arima"]]$auto_arima == TRUE){
+        message("ARIMA optimization...")
+        auto.arima(y_var_int, stepwise = TRUE
+                                , seasonal = parameter[["arima"]][["search_seasonal"]])
+      } else if(any(is.na(parameter[["arima"]]$pdq[4:6])) == FALSE){
+        suppressWarnings(Arima(y_var_int
+                                            , order = parameter[["arima"]]$pdq[1:3]
+                                            , seasonal = parameter[["arima"]][["pdq"]][4:6]
+                                            , method = "ML"))
+      } else {
+        suppressWarnings(Arima(y_var_int
+                                            , order = parameter[["arima"]]$pdq[1:3]
+                                            , method = "ML"))
+      }
+    }
+    , error = function(err){
+      suppressWarnings(Arima(y_var_int
+                             , order = c(0, 0, 0)
+                             , method = "ML"))
+    }
+  )
   return(arima_fit)
 }
