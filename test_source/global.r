77688dfd9c3277e3a837c5c26d79565f01a50a13
Poyser <obryan.poyser@sanofi.com>
2021-05-05 17:08:00 +0200

    change global

--- test_source/global.r
+++ test_source/global.r
@@ -4,15 +4,16 @@
 source_conf = list(source = "oc"
                  , date_cycle = "2021-05-01"
                  , db = list("full_sales"
-                             , "full_forecast"
+                             #, "full_forecast"
                              , "regressor"
-                             , "forecast_item_info")
-                 , countries = c("NO", "SE", "DK", "FI", "NL", "BE", "IN", "LV", "LT", "EE")
+                             #, "forecast_item_info"
+                             )
+                 #, countries = c("NO", "SE", "DK", "FI", "NL", "BE", "IN", "LV", "LT", "EE")
+                 , countries = "DK"
                  , filters = list(category = "HistoricalSales"
-                                  , cycle_category = "after_cleansing")
-                 , gbus = "GEM"
-                 , join_hist_forecast = T
-                 , do_prescribe = T)
+                                  , cycle_category = "before_cleansing")
+                 , gbus = c("GEM", "SPC", "CHC")
+                 , join_hist_forecast = T)
 
 parameter <- get_default_hyperpar()
 oc_data <- import_data(source_conf = source_conf)
