0dd90a14c305aea9ad04736a175c528702b66aaa
Poyser <obryan.poyser@sanofi.com>
2021-05-05 11:31:52 +0200

    function simplification and optimization

--- R/stat_analysis.R
+++ R/stat_analysis.R
@@ -1,18 +1,22 @@
-
-# GAM ####
-
-
-source_oc = list(source = "oc"
-            , date_cycle = "2021-03-01"
-            , db = c("full_sales", "full_forecast", "regressor", "forecast_item_info")
-            , countries = c("NO", "SE", "DK", "FI", "NL", "BE", "IN", "LV", "LT", "EE")
-            , gbus = "GEM"
-            , do_prescribe = T)
-
-oc_data <- import_data(source_conf = source_oc)
-
 # Functions ---------------------------------------------------------------
 
+#' Calculate Marginal Seasonal Effects
+#'
+#' @param .data tibble/data.frame
+#' @param min_date date: set mininum date.
+#' @param max_date date: set maximum date.
+#' @param robust_lm logical: whether or not to use robust regression.
+#' @param ci_me numeric: confidence interval for marginal effects.
+#' 
+#' @importFrom MASS rlm
+#' @import tidyverse
+#' @import ggeffects
+#' @import janitor
+#'
+#' @return
+#' @export
+#'
+#' @examples
 get_seas_me <- function(.data, min_date = NULL
                         , max_date = NULL, robust_lm = F
                         , ci_me = .95){
@@ -28,7 +32,6 @@ get_seas_me <- function(.data, min_date = NULL
     pull(z)
   
   if(robust_lm == T){
-    library(MASS)
     lm_reg <- match.fun("rlm")
   } else {
     lm_reg <- match.fun("glm")
@@ -56,218 +59,368 @@ get_seas_me <- function(.data, min_date = NULL
            , month_seas = factor(x, levels = month.abb)) %>% 
     dplyr::select(month_seas, everything(), -x, -group)
 }
-
-
-join_oc_data <- function(.oc_data, .key){
-  forecast_data <- .oc_data$knx_forecast %>% 
-    filter(key == .key) %>% 
-    pull(data) %>% 
-    .[[1]]
+#' Get GAM features
+#'
+#' @param oc_data tibble
+#' @param key string: name of the key to be processed.
+#' @param parameter list: parameters and hyperparameters.
+#' 
+#' @import tidyverse
+#' @import janitor
+#' @import gratia
+#' @import mgcv
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_insight_data <- function(oc_data, key, parameter){
+  key_in <- key
   
-  sales_data <- .oc_data$knx_sales %>% 
-    filter(key == .key)%>% 
-    pull(data) %>% 
-    .[[1]]
+  data_init <- oc_data$sales %>% 
+    filter(forecast_item == key_in)
+  data_init <- data_init[!rev(cumsum(rev(data_init$sales))==0), ]
+  date_vec <- data_init %>% 
+    filter(reg_name == "history") %>% 
+    pull(date) %>% 
+    unique()
+  data_init <- data_init %>% 
+    group_split(date <= max(date_vec), .keep = F)
   
-  forecast_data %>% 
-    dplyr::select(date_var, y_var) %>% 
-    distinct() %>% 
-    mutate(reg_name = "is_history", reg_value = 0)%>% 
-    bind_rows(forecast_data, .) %>% 
-    bind_rows(
-      sales_data %>% 
-        dplyr::select(date_var, y_var) %>% 
-        distinct() %>% 
-        mutate(reg_name = "is_history", reg_value = 1) %>% 
-        bind_rows(sales_data, .)
-      , .
-    ) %>%
+  
+  
+  hd <- data_init %>%
+    map(~{
+      pd <- prescribe_ts(.x, key = "forecast_item", y_var = "sales"
+                 , date_var = "date", reg_name = "reg_name"
+                 , reg_value = "reg_value", freq = 12, date_format = "ymd") %>% 
+      pull(data) %>% 
+      .[[1]] %>% 
+      validate_ts() %>% 
+      feature_engineering_ts() %>% 
+      dplyr::select(-any_of("history"))
+      
+      fit_in <- fit_ts(.data = pd, ts_model = "gam", parameter = parameter)
+      
+      fitted_in <- predict(fit_in, newdata = pd, se.fit = T) %>% 
+        as.data.frame() %>% 
+        as_tibble() %>% 
+        rowwise() %>% 
+        mutate(lwr = fit - 1.96*se.fit
+               , upr = fit + 1.96*se.fit) %>% 
+        ungroup() %>% 
+        janitor::clean_names()
+      
+      pd_ext <- pd %>% 
+        bind_cols(fitted_in)
+      
+      derivative_in <- tryCatch(
+        {
+          fit_in %>% 
+            gratia::derivatives(n = 100) %>% 
+            as_tibble() %>%
+            rename(trend = data, lwr = lower, upr = upper
+                   , fit_se = se, trend_deriv = derivative)
+        }
+        , error = function(err) {tibble(error = NA_real_)}
+      )
+      
+      seas_me <- pd_ext %>% 
+        mutate(y_var_d1 = tsibble::difference(y_var)) %>% 
+        na.omit() %>% 
+        get_seas_me()
+    
+      list(gam_fitted = pd_ext
+           , pd_data = pd
+           , fit = fit_in
+           , gam_trend_deriv = derivative_in
+           , seas_me = seas_me)
+  })
+  
+  
+  # GAM Forecast
+  
+  new_data <- hd[[1]]$pd_data %>% 
+    mutate(trend = get_trend_decay(y_var_length = length(hd[[2]]$pd_data$y_var)
+                                   , trend_decay = parameter$gam$trend_decay
+                                   , horizon = length(trend))) %>%
+    dplyr::select(-any_of(c("date_var", "y_var")))
+   # mutate(trend = length(unique(date_vec)) +  trend)
+
+  forecast_gam <- predict.gam(object = hd[[2]]$fit, newdata = new_data, se.fit = T) %>% 
+    dplyr::bind_cols() %>% 
+    janitor::clean_names() %>% 
+    mutate(lwr = fit - 1.96 * se_fit
+           , upr = fit + 1.96 * se_fit
+           , date = hd[[1]]$pd_data$date_var) %>% 
+    set_names(nm = paste0(names(.), "_gam")) %>% 
+    rename(forecast_gam = fit_gam)
+  
+  forecast_gam_deriv <- forecast_gam %>% 
+    dplyr::select(forecast_gam, date_gam) %>%
+    mutate(key = "fcst", reg_value = NA_real_, reg_name = "") %>% 
+    prescribe_ts(y_var = "forecast_gam"
+                 , reg_name = "reg_name"
+                 , reg_value = "reg_value"
+                 , key = "key"
+                 , date_var = "date_gam"
+                 , freq = 12
+                 , date_format = "ymd") %>% 
+    pull(data) %>% 
+    .[[1]] %>% 
     validate_ts() %>% 
     feature_engineering_ts() %>% 
-    clean_ts(winsorize_config = list(apply_winsorize = T, add_transformations = T)
-             , imputation_config = list(impute_method = "kalman"
-                                        , na_regressor = TRUE
-                                        , na_missing_dates = TRUE
-                                        , add_transformations = T)) %>% 
-    mutate(is_history = factor(is_history, levels = c(1, 0))) %>% 
-    group_by(is_history) %>% 
-    mutate(y_var_d1 = tsibble::difference(y_var, 1), .after = "y_var") %>% 
-    slice(-1) %>% 
-    ungroup()
-}
-
-
-seas_me_data <- join_oc_data(.oc_data = oc_data, .key = key) %>% 
-  group_nest(is_history) %>% 
-  mutate(data = map(data, ~get_seas_me(.x, robust_lm = F))) %>% 
-  unnest(data)
-
-graph_seas_me(seas_me_data)
-
-
-data_in <- oc_data$knx_sales %>%
-  filter(key =="BE: 115110") %>% 
-  pull(data) %>%
-  .[[1]] %>%
-  validate_ts() %>% 
-  feature_engineering_ts() %>% 
-  distinct()
-
-gam_fit <- data_in %>% 
-  fit_ts(.data = ., ts_model = "gam", parameter = parameter)
-
-gam_fitted <- predict(gam_fit, newdata = data_in, se.fit = T) %>% 
-  as.data.frame() %>% 
-  as_tibble() %>% 
-  rowwise() %>% 
-  mutate(lwr = fit - 1.96*se.fit
-         , upr = fit + 1.96*se.fit) %>% 
-  ungroup() %>% 
-  janitor::clean_names()
-
-data_in %>% 
-  bind_cols(gam_fitted) %>% 
-  ggplot()+
-  geom_ribbon(aes(date_var, ymin = lwr, ymax= upr)
-              , fill = "blue", alpha = .3)+
-  geom_line(aes(date_var, y_var))+
-  geom_line(aes(date_var, fit), col = "red")
-  
-gam_fitted <- predict_ts(fit = gam_fit, ts_model = "gam"
-           , optim_conf = optim_conf
-           , parameter = parameter
-           , add_fitted = T, .data = data_in)
-
-gam_derivative <- gam_fit %>% 
-  gratia::derivatives() %>% 
-  as_tibble() %>% 
-  rename(trend = data) %>% 
-  ggplot()+
-  geom_hline(yintercept = 0, linetype = "dashed")+
-  geom_ribbon(aes(trend, ymin = lower, ymax = upper), alpha = .2, fill = "red")+
-  geom_line(aes(trend, derivative))+
-  labs(x = "Time index (continuous)", y = "Trend derivative")+
-  theme_minimal()
-
-plotly::ggplotly(gam_derivative)
-
-
-
-graph_seas_me <- function(seas_me_data, has_forecast = F){
-  g1 <- seas_me_data %>% 
-    ggplot()+
-    geom_hline(yintercept = 0, linetype = "dotted", col = "red", size = 1)+
-    geom_linerange(aes(x = month_seas, ymin = conf_low, ymax = conf_high
-                       , col = is_history)
-                   , position = position_dodge(width = .3)
-                   #, col="black"
-                   )+
-    geom_point(aes(month_seas, predicted
-                   , col = is_history), position = position_dodge(width = .3)
-               , size = 3)+
-    theme_minimal()+
-    theme(axis.text = element_text(colour = "black", size = 12)
-          , axis.title = element_text(colour = "black", size = 15))+
-    labs(x = "", y = "Marginal Seasonal Effects")
-  
-    plotly::ggplotly(g1)
-}
-
-
-
-
-%>% 
-  #rename(is_history = group) %>% 
-  
+    mutate(trend = new_data$trend) %>% 
+    fit_ts(ts_model = "gam", parameter = parameter) %>% 
+    gratia::derivatives(n = 100) %>% 
+    as_tibble() %>%
+    rename(trend = data, lwr = lower, upr = upper
+           , fit_se = se, trend_deriv = derivative) %>% 
+    mutate(history = "forecast_gam")
   
 
-
-
-
-
-
-
+  # Features
   
-
-imp_kalman <- function(.data){
-  
-  if(all(is.na(.data$reg_name))){
-    .data %>% 
-      dplyr::select(date, sales, series_type) %>% 
-      distinct()
-  } else {
-    tryCatch(
-      {
-        kalman_int <- function(vec){
-          ts1 <- ts(vec, frequency = 12)
-          as.numeric(round(imputeTS::na_kalman(x = ts1), 0))
-        }
-        
-        fcst <- .data %>% 
-          filter(series_type %in% c(0, "forecast")) %>% 
-          dplyr::select(date, sales, series_type) %>% 
-          distinct()
-        
-        sales_new <- .data %>% 
-          filter(series_type %in% c(1, "history")) %>%
-          group_by(date) %>% 
-          summarise(sales = mean(sales)
-                    , reg_value = any(reg_value != 0)
-                    , .groups = "drop") %>% 
-          mutate(sales = case_when(
-            reg_value == T ~ NA_real_
-            , TRUE ~ sales)) %>% 
-          dplyr::select(-reg_value)
-        
-        sales_clean <- sales_new %>% 
-          pull(sales) %>%
-          kalman_int()
-        
-        
-        sales_new %>% 
-          mutate(sales = sales_clean
-                 , series_type = "history") %>% 
-          bind_rows(fcst)
-      }
-      , error = function(err) {
-        .data %>% 
-          dplyr::select(date, sales, series_type) %>% 
-          distinct()
-      }
+  hd[[1]]$gam_trend_deriv <- tryCatch(
+    {
+      hd[[1]]$gam_trend_deriv %>% 
+        mutate(trend = trend + length(unique(date_vec)))
+    }
+    , error = function(err) NULL
     )
+  
+  gam_fitted <- hd[[2]]$gam_fitted %>%
+    mutate(history = T) %>% 
+    bind_rows(hd[[1]]$gam_fitted) %>% 
+    replace_na(replace = list(history=F)) %>% 
+    left_join(forecast_gam, by = c("date_var" = "date_gam"))
+    
+  gam_deriv_trend <- hd[[2]]$gam_trend_deriv %>% 
+    mutate(history = "history") %>% 
+    bind_rows(hd[[1]]$gam_trend_deriv) %>% 
+    replace_na(replace = list(history="forecast")) %>% 
+    bind_rows(forecast_gam_deriv)
+  
+  seas_me <- hd[[2]]$seas_me %>% 
+    mutate(history = T) %>% 
+    bind_rows(hd[[1]]$seas_me) %>% 
+    replace_na(replace = list(history=F))
+  
+  # stats
+  
+  stats <- gam_fitted %>% 
+    filter(history == F) %>% 
+    dplyr::select(date_var, y_var, fit, forecast_gam) %>% 
+    mutate(y_var_cum = cumsum(y_var)
+           , fit_gam_cum = cumsum(forecast_gam)) %>% 
+    rowwise() %>% 
+    mutate(fit_diff = y_var - forecast_gam
+           , fit_diff_perc = round(y_var/forecast_gam - 1, 2)
+           , fit_cum_diff_perc = round(y_var_cum/fit_gam_cum - 1, 2)) %>% 
+    ungroup() %>% 
+    dplyr::select(-y_var, -fit, - forecast_gam)
+  
+  gam_fitted <- gam_fitted %>% 
+    left_join(stats, by = "date_var")
+  
+  
+  n_months_ahead_forecast <- length(stats$date_var)
+  cuts_month <- c(1, 6, 12, 24)
+  cuts_month <- cuts_month[cuts_month <= n_months_ahead_forecast]
+  
+  
+  summary_stats_1 <- stats %>% 
+    slice(cuts_month) %>% 
+    mutate(months_ahead = cuts_month) %>% 
+    dplyr::select(months_ahead, fit_cum_diff_perc)
+  
+  list(gam_fitted = gam_fitted
+       , gam_deriv_trend = gam_deriv_trend
+       , seas_me = seas_me
+       , summary_stats = summary_stats_1)
+}
+
+#' Generate graphics for time series insights
+#'
+#' @param insight_data tibble.
+#' @param graph_type string: {derivative, forecast, marginal}.
+#' @param conf list: graph options.
+#' 
+#' @import tidyverse
+#' @import shiny
+#' @import stringr
+#' @importFrom plotly ggplotly
+#' @importFrom plotly layout 
+#' @import mgcv
+#' @import tidyr
+#' 
+#' @return
+#' @export
+#'
+#' @examples
+get_graph_stat <- function(insight_data, graph_type, conf = list(min_limit_zero = F
+                                                                 , gam_forecast = F)){
+  
+  if(graph_type == "forecast"){
+    if(conf$min_limit_zero == T){
+      tmp <- insight_data$gam_fitted %>% 
+        mutate(across(.cols = c("fit", "lwr", "upr", "upr_gam", "lwr_gam", "forecast_gam"), ~ifelse(.x<0, 0, .x)))
+        scale_y_continuous(limits = c(0, max(insight_data$gam_fitted$upr))
+                           , n.breaks = 10, minor_breaks = NULL)
+    } else {
+      tmp <- insight_data$gam_fitted
+    }
+    
+    has_reg <- !all(str_detect(names(tmp), "date_var|y_var|^trend|_seas|fit|lwr|upr|history|cum|forecast_gam"))
+    
+    if(has_reg == T){
+      y_var_na <- tryCatch({
+        tmp %>% 
+          dplyr::select(-matches("date_var|fit|_seas|lwr|upr|history|trend|cum|forecast_gam")) %>% 
+          mutate(across(2:last_col(), .fns = ~.x!=0)) %>%
+          mutate(ind = rowSums(.[c(2:ncol(.))])) %>% 
+          mutate(ind = ifelse(ind!=0, y_var, NA_real_)) %>% 
+          pull(ind)
+      }, error = function(err) pull(tibble(y_var_na = NA, .rows = nrow(tmp)))
+      )
+      
+      label <- tmp %>% 
+        dplyr::select(-matches("y_var|fit|_seas|lwr|upr|history|trend|cum|forecast_gam")) %>% 
+        pivot_longer(cols = 2:last_col()) %>% 
+        filter(value !=0) %>% 
+        group_by(date_var) %>% 
+        summarise(label = paste0(name, collapse = ", "))
+      
+      tmp <- tmp %>%
+        mutate(y_var_na = y_var_na) %>% 
+        left_join(label, by = "date_var")
+    }
+    
+    
+    # limits
+    
+    g_lims <- tmp %>% 
+      #dplyr::select(matches("lwr|upr|y_var")) %>% 
+      dplyr::select(any_of(c("lwr", "upr", "y_var"))) %>%
+      unlist() %>% 
+      na.omit() %>% 
+      range()
+    
+    g_lims[1] <- g_lims[1]*.8
+    g_lims[2] <- g_lims[2]*1.2
+    
+    g_tmp <- tmp %>% 
+      ggplot()+
+      geom_ribbon(aes(date_var, ymin = lwr, ymax= upr, fill = history), alpha = .2)+
+      geom_line(aes(date_var, y_var, col = history))+
+      geom_line(aes(date_var, fit, col = history), linetype ="dashed", size = 1)+
+      scale_x_date(date_breaks = "4 month", date_labels = "%b-%y", minor_breaks = NULL)+
+      labs(y = "Sales", x = "")+
+      theme_minimal()+
+      theme(axis.text.x = element_text(angle = 90)
+            , legend.position="bottom")+
+      geom_hline(yintercept = 0, linetype = "dotted") +
+      scale_y_continuous(n.breaks = 10, minor_breaks = NULL)+
+      theme(legend.position =  "none")
+    
+    if(conf$gam_forecast == T){
+      g_tmp <- g_tmp + 
+        # geom_ribbon(aes(date_var, ymin = lwr_gam
+        #                 , ymax = upr_gam), fill = "purple", alpha = .2)+
+        geom_line(aes(date_var, forecast_gam), col = "green")
+    }
+    
+    if(has_reg == T){
+      g_tmp <- g_tmp +
+        geom_point(aes(date_var, y_var_na, shape = label)
+                   , show.legend = T, na.rm = TRUE)+
+        scale_shape_discrete(na.translate = F)
+    }
+    
+    ggplotly(g_tmp, dynamicTicks = F, layerData = T) %>% 
+      layout(legend = list(orientation = "h", x = .7, y = 0.96)
+             , yaxis = list(autorange = T))
+    
+  } else if(graph_type == "derivative"){
+    g_tmp <- insight_data$gam_deriv_trend %>% 
+      ggplot()+
+      geom_hline(yintercept = 0, linetype = "dashed")+
+      geom_ribbon(aes(trend, ymin = lwr, ymax = upr
+                      , fill = history), alpha = .2)+
+      geom_line(aes(trend, trend_deriv, col = history))+
+      labs(x = "Time index (continuous)", y = "Trend derivative")+
+      theme_minimal()+
+      theme(legend.position = "none")
+    
+    ggplotly(g_tmp)
+    
+  } else if(graph_type == "seas_me"){
+    g_tmp <- insight_data$seas_me %>% 
+      ggplot()+
+      geom_hline(yintercept = 0, linetype = "dotted", col = "red", size = 1)+
+      geom_linerange(aes(x = month_seas, ymin = conf_low, ymax = conf_high
+                         , col = history)
+                     , position = position_dodge(width = .3)
+                     #, col="black"
+                     )+
+      geom_point(aes(month_seas, predicted
+                     , col = history), position = position_dodge(width = .3)
+                 , size = 2)+
+      theme_minimal()+
+      theme(legend.position="none")+
+      # theme(axis.text = element_text(colour = "black", size = 12)
+      #       , axis.title = element_text(colour = "black", size = 15))+
+      labs(x = "", y = "Marginal Seasonal Effects")
+    
+    ggplotly(g_tmp) %>% 
+      layout(legend = list(orientation = "h", x = .45, y = .96))
   }
+  
+}
+
+#' Internal GAM parameter
+#'
+#' @param parameter list: parameters and hyperparameters.
+#' @param values_list list: parameter modifiers.
+#'
+#' @return
+#'
+#' @examples
+react_gam_par <- function(parameter, values_list){
+  if(values_list$k != -1){
+    parameter$gam$smoothed_features$trend$k <- values_list$k
+  }
+  
+  if(values_list$trend_decay !=1){
+    parameter$gam$trend_decay <- values_list$trend_decay
+  }
+  
+  if(any(is.null(values_list$exc_features)) == F){
+    parameter$gam$excluded_features <- c(values_list$exc_features)
+  }
+  return(parameter)
 }
 
 
-input_data2 <- input_data %>% 
-  #slice(1:10) %>% 
-  mutate(data = map(data, ~imp_kalman(.x)))
-
-
-
-active_items <- fii %>% 
-  filter(is_excluded == F) %>% 
-  dplyr::select(forecast_item, lifecycle, is_excluded)
-
-platinum <- fii %>% 
-  filter(p2w_strategy == "Platinum", is_excluded == F) %>% 
-  pull(forecast_item)
-
-forecastable <- fii %>% 
-  filter(is_excluded == F, forecastability == T
-         #, str_detect(lifecycle, "Switch-Out|EOL", negate = T)
-  ) %>% 
-  pull(forecast_item)
-
-
-
-# Analysis ----------------------------------------------------------------
-
-d1$data[[4]] %>% 
-  validate_ts() %>% 
-  feature_engineering_ts() %>% 
-  clean_ts(winsorize_config = list(apply_winsorize = T
-                                   , add_transformations = T)
-           , imputation_config = list(hierarchical_seas = T))
-
-
+#' Internal modification of statistical data
+#'
+#' @param insight_data tibble/data.frame
+#' @param date date: set minimum date.
+#'
+#' @return
+#'
+#' @examples
+mod_stat_data <- function(insight_data, date = NULL){
+  # if(date == ""){
+  #   date <- "1899-01-01"
+  # }
+  
+  tryCatch({
+    data_hist <- insight_data$gam_fitted %>% 
+      filter(date_var >= date)
+    
+    insight_data$gam_fitted <- data_hist
+    return(insight_data)
+  }
+  , error = function(err) insight_data)
+}
