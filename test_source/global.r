262aa37d1b375ae66c3342827412869d9b00ab7d
Poyser <obryan.poyser@sanofi.com>
2021-05-14 12:10:48 +0200

    global data for dashboard

--- test_source/global.r
+++ test_source/global.r
@@ -4,27 +4,32 @@
 source_conf = list(source = "oc"
                  , date_cycle = "2021-05-01"
                  , db = list("full_sales"
-                             #, "full_forecast"
+                             , "full_forecast"
                              , "regressor"
                              #, "forecast_item_info"
                              )
-                 #, countries = c("NO", "SE", "DK", "FI", "NL", "BE", "IN", "LV", "LT", "EE")
-                 , countries = "DK"
+                 , countries = c("NO", "SE", "DK", "FI", "NL", "BE", "IN", "LV", "LT", "EE")
+                 #, countries = "RU"
                  , filters = list(category = "HistoricalSales"
                                   , cycle_category = "before_cleansing")
-                 , gbus = c("GEM", "SPC", "CHC")
+                 , gbus = c("GEM")
                  , join_hist_forecast = T)
 
 parameter <- get_default_hyperpar()
 oc_data <- import_data(source_conf = source_conf)
 forecast_item_list <- oc_data$sales$forecast_item %>% 
-  unique()
+  unique() %>% 
+  sort()
 
 insight_data <- get_insight_data(oc_data = oc_data
-                                 , key = "NL: 569438"
+                                 , key = "FI: 474452"
                                  , parameter = get_default_hyperpar())
 
-get_graph_stat(insight_data = insight_data, graph_type = "derivative")
+get_graph_stat(insight_data = insight_data, graph_type = "seas_me")
 
 
 
+
+
+ get_tables(insight_data = insight_data, table_type = "year_agg")
+ 
